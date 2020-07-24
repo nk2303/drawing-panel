87994ad60aa2ee33e4dfe110488f6e19405e7981
nk2303 <ngankhong03@gmail.com>
2020-07-23 21:37:39 -0700

    fix color

--- Doodle.java
+++ Doodle.java
@@ -3,20 +3,20 @@ import java.awt.*;
 public class Doodle {
     public static void main (String[] args) {
         DrawingPanel panel = new DrawingPanel(500,500);
-        Graphics face = panel.getGraphics();
-        Graphics body = panel.getGraphics();
-        Graphics hair = panel.getGraphics();
-        face.setColor(Color.RED);
-        body.setColor(Color.BLUE);
-        hair.setColor(Color.BLACK);
-        face.drawOval(200, 77, 100, 100);
-        hair.drawOval(230, 120, 10, 10);
-        hair.drawOval(270, 120, 10, 10);
-        hair.fillOval(200, 70, 100, 20);
-        body.fillRect(190, 180, 120, 200);
-        body.fillRect(190, 180, 120, 200);
-        body.fillRect(200, 360, 40, 60);
-        body.fillRect(260, 360, 40, 60);
+        Graphics graph = panel.getGraphics();
+        // Graphics body = panel.getGraphics();
+        // Graphics hair = panel.getGraphics();
+        graph.setColor(Color.RED);
+        graph.drawOval(200, 77, 100, 100);
+        graph.setColor(Color.BLUE);
+        graph.fillRect(190, 180, 120, 200);
+        graph.fillRect(190, 180, 120, 200);
+        graph.fillRect(200, 360, 40, 60);
+        graph.fillRect(260, 360, 40, 60);
+        graph.setColor(Color.BLACK);
+        graph.drawOval(230, 120, 10, 10);
+        graph.drawOval(270, 120, 10, 10);
+        graph.fillOval(200, 70, 100, 20);
 
     }
     
