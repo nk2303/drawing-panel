2107540b8fd874ba9d403fc358206d588dce0afe
nk2303 <ngankhong03@gmail.com>
2020-07-23 21:31:31 -0700

    lol

--- Doodle.java
+++ Doodle.java
@@ -3,11 +3,20 @@ import java.awt.*;
 public class Doodle {
     public static void main (String[] args) {
         DrawingPanel panel = new DrawingPanel(500,500);
-        Graphics doodle = panel.getGraphics();
-        doodle.setColor(Color.BLUE);
-        doodle.fillRect(17, 45, 139, 241);
-        doodle.drawOval(234, 77, 100, 100);
-        doodle.fill3DRect(150, 300, 100, 100, true);
+        Graphics face = panel.getGraphics();
+        Graphics body = panel.getGraphics();
+        Graphics hair = panel.getGraphics();
+        face.setColor(Color.RED);
+        body.setColor(Color.BLUE);
+        hair.setColor(Color.BLACK);
+        face.drawOval(200, 77, 100, 100);
+        hair.drawOval(230, 120, 10, 10);
+        hair.drawOval(270, 120, 10, 10);
+        hair.fillOval(200, 70, 100, 20);
+        body.fillRect(190, 180, 120, 200);
+        body.fillRect(190, 180, 120, 200);
+        body.fillRect(200, 360, 40, 60);
+        body.fillRect(260, 360, 40, 60);
 
     }
     
